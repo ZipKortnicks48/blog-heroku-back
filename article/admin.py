109a3c155e230b72eb9a998e90f60b224a5756f1
Александр Сырокласов <SiroklasovA@rkvv.local>
2020-02-10 08:24:29 +0300

    register models

--- article/admin.py
+++ article/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 # -*- coding: utf-8 -*-
+from django.contrib import admin
+from .models import Content,Subcategory,Category
+# Register your models here.
+admin.site.register(Content)
+admin.site.register(Subcategory)
+admin.site.register(Category)
